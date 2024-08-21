9aff9ca9bb45cc98a85c0de98f737c591cc55009
Coder-Blue <trananhquan1009@gmail.com>
2024-08-21 23:15:20 +0700

    Fix Git Error

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-import { convertToCoreMessages, streamText } from "ai";
+import { streamText } from "ai";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
-=======
-import {
-  GoogleGenerativeAI,
-  HarmCategory,
-  HarmBlockThreshold,
-} from "@google/generative-ai";
-import { GoogleGenerativeAIStream, Message, StreamingTextResponse } from "ai";
->>>>>>> 39f574211871bd38a767e60931e8f3e1a1d1544c
 import { NextResponse } from "next/server";
 
 export const runtime = "edge";
 
 const apiKey = process.env.GEMINI_API_KEY || "";
 
-<<<<<<< HEAD
 const google = createGoogleGenerativeAI({
   apiKey,
 });
@@ -40,51 +30,11 @@ const model = google("gemini-1.5-pro", {
     },
   ],
 });
-=======
-const genAI = new GoogleGenerativeAI(apiKey);
-
-const buildGoogleGenAIPrompt = (messages: Message[]) => ({
-  contents: messages
-    .filter(
-      (message) => message.role === "user" || message.role === "assistant",
-    )
-    .map((message) => ({
-      role: message.role === "user" ? "user" : "model",
-      parts: [{ text: message.content }],
-    })),
-});
-
-const generationConfig = {
-  temperature: 0.9,
-  topP: 1,
-  topK: 0,
-  maxOutputTokens: 2048,
-  responseMimeType: "text/plain",
-};
-
-const safetySettings = [
-  {
-    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-  },
-];
->>>>>>> 39f574211871bd38a767e60931e8f3e1a1d1544c
 
 export async function POST(req: Request) {
   try {
     const { messages } = await req.json();
+    const data = JSON.stringify(messages);
 
     if (!apiKey)
       return new NextResponse("Không tìm thấy API cho Google Gemini", {
@@ -93,12 +43,11 @@ export async function POST(req: Request) {
     if (!messages)
       return new NextResponse("Không tìm thấy lệnh", { status: 400 });
 
-<<<<<<< HEAD
     const result = await streamText({
       model: model,
       system:
         "Bạn là một trợ lý AI thân thiện, luôn trả lời câu hỏi theo cách dễ hiểu nhưng bài bản tạo cảm giác như nói chuyện với một giáo viên cấp 3 chuyên nghiệp.",
-      messages: convertToCoreMessages(messages),
+      prompt: data,
       temperature: 1,
       topP: 0.95,
       topK: 64,
@@ -106,19 +55,6 @@ export async function POST(req: Request) {
     });
 
     return result.toDataStreamResponse();
-=======
-    const geminiStream = await genAI
-      .getGenerativeModel({
-        model: "gemini-1.0-pro",
-        generationConfig,
-        safetySettings,
-      })
-      .generateContentStream(buildGoogleGenAIPrompt(messages));
-
-    const stream = GoogleGenerativeAIStream(geminiStream);
-
-    return new StreamingTextResponse(stream);
->>>>>>> 39f574211871bd38a767e60931e8f3e1a1d1544c
   } catch (error) {
     console.log("[CONVERSATION ERROR]", error);
     return new NextResponse("Lỗi nội bộ", { status: 500 });
--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   experimental: {
     staleTimes: {
       dynamic: 30,
     },
   },
-=======
->>>>>>> 39f574211871bd38a767e60931e8f3e1a1d1544c
   images: {
     remotePatterns: [
       {
